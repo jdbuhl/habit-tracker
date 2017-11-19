c055c138c3da43254aee43fadb27fcd0957c3fbb
Jessica D'Andrea <jdandrea11@gmail.com>
2017-11-18 19:16:55 -0600

    Fix props map issue

--- src/components/Habits.js
+++ src/components/Habits.js
@@ -3,9 +3,10 @@ import { Habit } from './IndividualHabit';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export const Habits = (props) => {
+  const data = props.habits || [];
   return (
     <div>
-    {props.habits.map(habit =>
+    {data.map(habit =>
       <MuiThemeProvider>
         <Habit onRemove={props.onRemove} onClick={props.onClick} habit={habit} />
       </MuiThemeProvider>
